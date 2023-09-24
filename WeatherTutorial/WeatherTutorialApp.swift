fc811fe0c47f8551adbede0fbcddfca461e72dcd
Limitless <q@prismatica.cards>
2023-09-23 20:00:56 -0500

    Initial Commit

---
+++ WeatherTutorial/WeatherTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherTutorialApp.swift
+//  WeatherTutorial
+//
+//  Created by Wuqiong Fan on 9/23/23.
+//
+
+import SwiftUI
+
+@main
+struct WeatherTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
